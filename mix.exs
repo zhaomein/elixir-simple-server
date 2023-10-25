aebf3c459e053912f1b1a9c7cc66603ed2299f0b
RobertZhao <niemtt.dev@gmail.com>
2023-10-25 15:00:20 +0700

    Update change

--- mix.exs
+++ mix.exs
@@ -26,6 +26,7 @@ defmodule ChatServer.MixProject do
       {:poison, "~> 5.0"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:skn_bot, git: "https://github.com/skygroup2/skn_bot.git", branch: "main"},
+      {:skn_lib, git: "https://github.com/skygroup2/skn_lib.git", branch: "main", override: true},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
